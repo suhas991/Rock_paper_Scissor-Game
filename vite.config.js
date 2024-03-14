d8713398ea433dad8e92a3a756539cf796975089
suhas991 <suhas4497@gmail.com>
2024-03-14 11:41:45 +0530

    4TH commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Rock_paper_Scissor-Game",
+  base: "/Rock_paper_Scissor-Game/",
   plugins: [react()],
 })
